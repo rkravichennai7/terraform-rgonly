aa79dbcc5fcb17f522c21c4685dd9b0767fbc231
rkravichennai7 <rkravichennai7@gmail.com>
2025-07-26 12:05:58 +0530

    only rg creation

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "84382787-1367-4937-befb-9757f44087ef"
+}
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "rg_name" {
+    default = "ravi987"
+}
+variable "rg_location" {
+    default = "central india"
+}
